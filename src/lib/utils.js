9216c676dfd1dd21c327a6db216e9b71c4abb82c
benniBoi <bharrington11@gmail.com>
2019-10-19 11:23:04 -0400

    Add utils 'get' and 'uuid'
    UUID used currently to generate unique identifiers

---
+++ src/lib/utils.js
@@ -0,0 +1,21 @@
+const get = (obj, path, defaultValue) => {
+  const result = String.prototype.split.call(path, /[,[\].]+?/)
+    .filter(Boolean)
+    .reduce((res, key) => (res !== null && res !== undefined) ? res[key] : res, obj);
+  return (result === undefined || result === obj) ? defaultValue : result;
+};
+
+const uuid = () => {
+  let uuid = '', i, random;
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+
+    if (i == 8 || i == 12 || i == 16 || i == 20) {
+      uuid += '-';
+    }
+    uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+  }
+  return uuid;
+};
+
+export { uuid, get};
